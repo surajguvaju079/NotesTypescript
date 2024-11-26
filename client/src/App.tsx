4cfd2da0de80f38fca3fe511d8d3edd7ee15c18c
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2024-11-26 11:48:46 +0545

    client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-400">Hi</div>
+      <input className="input input-primary"></input>
+      <button className="btn btn-circle btn-success"></button>
+    </>
+  );
+}
+
+export default App;
