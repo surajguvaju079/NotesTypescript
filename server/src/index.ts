76a5a988c5ef330f786ce5dd9e8c874b029885aa
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2024-11-26 12:45:41 +0545

    server setup

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = undefined;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.listen(port, () => {
+  console.log("Server is listening ", port);
+});
