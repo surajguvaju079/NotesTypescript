37c15890d5e51320f1f03feb99f391b8d1170378
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2024-11-29 14:13:47 +0545

    data added

--- server/src/controller/noteController.ts
+++ server/src/controller/noteController.ts
@@ -1,6 +1,15 @@
 import Note from "../model/note";
 
-export const getNote = async (req, res) => {
+export const getNote = async (
+  req: { body: { user: string; email: string } },
+  res: {
+    status: (arg0: number) => {
+      (): any;
+      new (): any;
+      json: { (arg0: { success: boolean; message: string }): any; new (): any };
+    };
+  }
+) => {
   const { user, email } = req.body;
   const note = Note.create({ title: user, note: email });
   console.log(note);
