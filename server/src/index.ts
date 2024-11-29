70fd2a2cc8e81a1077cac40c65db1362a787ceba
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2024-11-29 13:43:25 +0545

    database setup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 const app = express();
-const port = undefined;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-app.listen(port, () => {
-  console.log("Server is listening ", port);
-});
+if (!process.env.MONGO_DB_URI) {
+  throw new Error("MONGO_DB_URI is not defined in environment variables.");
+}
+
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("Database is connected...");
+    app.listen(port, () => {
+      console.log(`Server is listening at port:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1); // Exit the process if database connection fails
+  });
