0c9f52b1a90767d5f5344845a44c4684ff4b2b7f
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2024-11-29 15:18:52 +0545

    dot env file solved

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
-
+import Note from "./model/note";
 const app = express();
-app.get("/", (req, res) => {
-  res.send("hi man");
+app.get("/", async (req, res) => {
+  const note = await Note.find().exec();
+  res.status(200).json({ note });
 });
 export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,14 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-if (!process.env.MONGO_DB_URI) {
+if (!process.env.MONGO_DB_URI_PASSWORD) {
   throw new Error("MONGO_DB_URI is not defined in environment variables.");
 }
 
 mongoose
-  .connect(process.env.MONGO_DB_URI)
+  .connect(
+    `mongodb+srv://surajguvaju0:${process.env.MONGO_DB_URI_PASSWORD}@cluster0.u4wiv.mongodb.net/cool_notes_app?retryWrites=true&w=majority&appName=Cluster0`
+  )
   .then(() => {
     console.log("Database is connected...");
     app.listen(port, () => {
