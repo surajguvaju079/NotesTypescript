731a4a551871a49adb8fcd32c2ec67c43230b727
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2024-12-03 09:15:41 +0545

    getone and getall notes api done

--- server/src/controller/noteController.ts
+++ server/src/controller/noteController.ts
@@ -5,7 +5,17 @@ export const getNotes: RequestHandler = async (req, res, next) => {
   try {
     //throw Error("Bazinga");
     const notes = await Notes.find().exec();
-    res.status(200).json({ notes });
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getNote: RequestHandler = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const noteExist = await Notes.findById(id).exec();
+    if (noteExist) res.status(201).json(noteExist);
   } catch (error) {
     next(error);
   }
--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -3,5 +3,6 @@ import * as NotesController from "../controller/noteController";
 const router = express.Router();
 router.get("/", NotesController.getNotes);
 router.post("/create", NotesController.createNotes);
+router.get("/getone/:id", NotesController.getNote);
 
 export default router;
